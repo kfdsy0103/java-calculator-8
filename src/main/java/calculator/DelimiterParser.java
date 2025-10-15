f52aa6f4293597851acb49f87e53f13e75fd51eb
pywoo <kfdsy0103@naver.com>
2025-10-15 21:39:29 +0900

    feat: 커스텀 구분자 파싱 기능 구현

---
+++ src/main/java/calculator/DelimiterParser.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class DelimiterParser {
+
+    public String getCustomDelimiter(String input) {
+        int index = input.indexOf("\\n");
+        if (index == -1) {
+            throw new IllegalArgumentException();
+        } else {
+            return input.substring(2, index - 1);
+        }
+    }
+}
