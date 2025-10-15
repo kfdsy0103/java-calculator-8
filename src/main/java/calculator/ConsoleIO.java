a57cfbad59b7e023f52890d006aae77e10e33a86
pywoo <kfdsy0103@naver.com>
2025-10-15 21:37:56 +0900

    feat: 결과 출력 기능 구현

--- src/main/java/calculator/ConsoleIO.java
+++ src/main/java/calculator/ConsoleIO.java
@@ -1,11 +1,16 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.math.BigInteger;
 
 public class ConsoleIO {
 
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
+    }
+
+    public void displayOutput(BigInteger result) {
+        System.out.println("결과 : " + result);
     }
 }
