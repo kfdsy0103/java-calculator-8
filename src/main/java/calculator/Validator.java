d65d7a1314522ead03dcbfdf707eee65c6f63afb
pywoo <kfdsy0103@naver.com>
2025-10-15 21:41:16 +0900

    feat: 검증 기능 구현

---
+++ src/main/java/calculator/Validator.java
@@ -0,0 +1,25 @@
+package calculator;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class Validator {
+
+    // 띄어쓰기가 포함된 숫자, 공백은 생성자를 통해 검증 (NumberFormatException)
+    // 0과 음수는 조건문으로 검증
+    public boolean validate(List<String> tokens) {
+        try {
+            for (String token : tokens) {
+                BigInteger number = new BigInteger(token);
+                if (number.equals(BigInteger.ZERO)) {
+                    return false;
+                } else if (number.compareTo(BigInteger.ZERO) < 0) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
